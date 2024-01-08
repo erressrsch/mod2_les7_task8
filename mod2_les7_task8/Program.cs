e05e8f1fc0429eca9c072eb4f826fe2a8503d6a2
Szymon Chyc <erressrsch@gmail.com>
2024-01-08 23:17:53 +0100

    Add project files.

---
+++ mod2_les7_task8/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace mod2_les7_task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Math exam result: ");
+            int mathResult = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your Physics exam result: ");
+            int physicResult = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Eneter your Chemistry exam result: ");
+            int chemistryResult = int.Parse(Console.ReadLine());
+
+            if (mathResult > 70 || physicResult > 55 || chemistryResult > 45 && (mathResult + physicResult + chemistryResult) > 180)
+            {
+                Console.WriteLine("Candidate admitted to recruitment");
+            }
+            else if ((mathResult + physicResult) > 150)
+            {
+                Console.WriteLine("Candidate admitted to recruitment");
+            }
+            else if ((mathResult + physicResult) > 150)
+            {
+                Console.WriteLine("Candidate admitted to recruitment");
+            }
+            else
+            {
+                Console.WriteLine("The candidate is not admitted to recruitment");
+            }
+        }
+    }
+}
